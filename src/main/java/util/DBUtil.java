5b56bb03c13823a67381f0930cc74176da89568f
mary <2723253023@qq.com>
2023-12-04 19:53:47 +0800

    修改数据库，用户名root,密码123456

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class DBUtil {
 
 	public static String username = "root";
-	public static String password = "sa";
-	public static String url = "jdbc:mysql://localhost:3306/books2?characterEncoding=utf-8";
+	public static String password = "123456";
+	public static String url = "jdbc:mysql://localhost:3306/gardens?characterEncoding=utf-8";
 	
 	static{
 		try {
