7bf579b18bde6c145c89b1fc5ef987ac322bb1fc
ReceiverLillian <1879282062@qq.com>
2023-12-04 15:16:15 +0800

    Initial commit :数据库名是gardens，测试表名是test

---
+++ src/main/java/ConnectDatabase.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+
+public class ConnectDatabase {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/gardens";
+        String username = "root";
+        String password = "123456";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM test");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
