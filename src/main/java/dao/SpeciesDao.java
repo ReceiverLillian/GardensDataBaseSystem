df7789e02d97b8e447a4272a475681a93335031b
ReceiverLillian <1879282062@qq.com>
2023-12-10 19:40:37 +0800

    管理员修改植物信息界面

--- src/main/java/dao/SpeciesDao.java
+++ src/main/java/dao/SpeciesDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import bean.Species;
 import bean.SpeciesBean;
 import util.DBUtil;
 
@@ -41,5 +42,65 @@ public class SpeciesDao {
         }
 
         return speciesBean;
+    }
+
+    public Species selectPlantById(int id) {//含外表字段版
+        Connection conn = DBUtil.getConnectDb();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Species species = new Species();
+
+        String query = "SELECT s.species_name, s.species_othername, s.species_morph, s.species_tech, s.species_appl, "
+                + "f.family_name, g.genus_name "
+                + "FROM species s "
+                + "JOIN genus_species gs ON s.species_id = gs.species_id "
+                + "JOIN genus g ON gs.genus_id = g.genus_id "
+                + "JOIN family_genus fg ON g.genus_id = fg.genus_id "
+                + "JOIN family f ON fg.family_id = f.family_id "
+                + "WHERE s.species_id = ?";
+
+        try {
+
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                /*String speciesName = rs.getString("species_name");
+                String speciesOtherName = rs.getString("species_othername");
+                String speciesMorph = rs.getString("species_morph");
+                String speciesTech = rs.getString("species_tech");
+                String speciesAppl = rs.getString("species_appl");
+                String familyName = rs.getString("family_name");
+                String genusName = rs.getString("genus_name");*/
+
+                species.setSpeciesName(rs.getString("species_name"));
+                species.setSpeciesOtherName(rs.getString("species_othername"));
+                species.setSpeciesMorph(rs.getString("species_morph"));
+                species.setSpeciesTech(rs.getString("species_tech"));
+                species.setSpeciesAppl(rs.getString("species_appl"));
+                species.setFamilyName(rs.getString("family_name"));
+                species.setGenusName(rs.getString("genus_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return species;
     }
 }
